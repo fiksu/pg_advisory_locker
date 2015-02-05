90a7b34042068589e7ffb3479cd5b58e5125f48f
David Hofer <hofer@fiksu.com>
2015-02-05 16:08:47 -0500

    bundle exec rake rails:update

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'fde75b7d949e71af1b40942b9cfb2254f0459bd29b330724aa091776d48d6102a4eb293cef639faee2e96c30499fddaf5fd23c8ed75d332ceaff68c951ed8b66'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '0b4810c26308e3664aa1d0a5d45aa6fb892663cada2a2fcf5a2860cd7af9f814309335cf1fe3925b901736b01006f9d10ae278f92f203417c717fb766d30b00e'
